96f8cddbc61ced6e6a6b6ecedcf61bdce8d7b323
alu0100288216 <alu0100288216@ull.edu.es>
2015-03-11 22:15:16 +0000

    gulpfile actualizado.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
-var gulp    = require('gulp');
+var gulp    = require('gulp')
+    gutil   = require('gulp-util'),
+    uglify  = require('gulp-uglify'),
+    concat  = require('gulp-concat');
+var del     = require('del');
+var karma   = require('gulp-karma');
+var minifyHTML = require('gulp-minify-html');
+var minifyCSS  = require('gulp-minify-css');
 var shell = require('gulp-shell');
+
+gulp.task('minify', function () {
+  gulp.src('csv.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('minified'))
+
+gulp.src('./index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./minified/'))
+
+gulp.src('./*.css')
+    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(gulp.dest('./minified/'))
+});
+
+gulp.task('clean', function(cb) {
+  del(['minified/*'], cb);
+});
 
 gulp.task('default', ['server']);
 
@@ -10,5 +35,16 @@ gulp.task('server', function () {
 
 gulp.task('open', function() {
   return gulp.src('').
-           pipe(shell("open https://github.com/crguezl/how-jquery-works-tutorial/tree/getallparams"));
+           pipe(shell("open https://alu0100288216.github.io"));
+});
+
+gulp.task('test', function() {
+  return gulp.src([])
+           .pipe(karma({
+             configFile: 'karma.conf.js',
+             action: 'run'
+           }))
+           .on('error',function(err){
+             throw err;
+           });
 });
