006a8a27770bd7e9ba772f45f2ec6b7ad6197cc4
Adrian <acglez10@gmail.com>
2015-03-18 18:44:12 +0000

    problema generar datos.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var expressLayouts = require('express-ejs-layouts');
+var $ = require('jquery');
+var _ = require('underscore');
 
 app.set('port', (process.env.PORT || 3000));
 
@@ -12,22 +14,59 @@ app.set('view engine', 'ejs');
 
 app.use(expressLayouts);
 //app.use(express.static('public'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'P_CSV' });
 })
 
-app.get('/csv-pl', function (req, res) {
-  var isAjaxRequest = req.xhr;
-  console.log(isAjaxRequest);
-  if (isAjaxRequest) {
-    console.log(req.query);
-    res.send('{"answer": "Server responds: hello world!"}')
+app.get('/', function (req, res) {
+  var result;
+  var original = document.getElementById("original");
+  var temp = original.value;
+  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
+  var lines = temp.split(/\n+\s*/);
+  var commonLength = NaN;
+  var r = [];
+  // Template using underscore
+  /*var row = "<% _.each(items, function(name) { %>"     +
+            "                    <td><%= name %></td>" +
+            "              <% }); %>";
+*/
+  var row = document.getElementById("tabla");
+  for(var t in lines) {
+    var temp = lines[t];
+    var m = temp.match(regexp);
+    var result = [];
+    var error = false;
+
+    if (m) {
+      if (commonLength && (commonLength != m.length)) {
+        alert('ERROR! row <'+temp+'> has '+m.length+' items!');
+        error = true;
+      }
+      else {
+        commonLength = m.length;
+        error = false;
+      }
+      for(var i in m) {
+        var removecomma = m[i].replace(/,\s*$/,'');
+        var remove1stquote = removecomma.replace(/^\s*"/,'');
+        var removelastquote = remove1stquote.replace(/"\s*$/,'');
+        var removeescapedquotes = removelastquote.replace(/\\"/,'"');
+        result.push(removeescapedquotes);
+      }
+      var tr = error? '<tr class="error">' : '<tr>';
+      r.push(tr+_.template(row, {items : result})+"</tr>");
+    }
+    else {
+      error = true;
+    }
   }
-  else {
-    res.send('not an ajax request');
-  }
+  r.unshift('<p>\n<table class="center" id="result">');
+  r.push('</table>');
+  //alert(r.join('\n')); // debug
+  finaltable.innerHTML = r.join('\n');
 });
 
 var server = app.listen(app.get('port'), function () {
--- csv.js
+++ csv.js
@@ -2,60 +2,18 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
 $(document).ready(function() {
-   $("button").click(function() {
-     calculate();
+    $("button").click(function () {
+        if (window.localStorage) localStorage.original = document.getElementById("original").value;
+        console.log(document.getElementById("original").value);
+        $.get("/", { input: document.getElementById("original").value }, function (data) {
+            var tabla = document.getElementById("tabla").innerHTML;
+            $("#finaltable").html(_.template(tabla, { rows: data.rows }));
+           console.log(_.template(tabla, { rows: data.rows }));
+       }, 'json');
    });
  });
 
-function calculate() {
-  var result;
-  var original       = document.getElementById("original");
-  var temp = original.value;
-  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
-  var lines = temp.split(/\n+\s*/);
-  var commonLength = NaN;
-  var r = [];
-  // Template using underscore
-  var row = "<% _.each(items, function(name) { %>"     +
-            "                    <td><%= name %></td>" +
-            "              <% }); %>";
 
-  if (window.localStorage) localStorage.original  = temp;
-
-  for(var t in lines) {
-    var temp = lines[t];
-    var m = temp.match(regexp);
-    var result = [];
-    var error = false;
-
-    if (m) {
-      if (commonLength && (commonLength != m.length)) {
-        alert('ERROR! row <'+temp+'> has '+m.length+' items!');
-        error = true;
-      }
-      else {
-        commonLength = m.length;
-        error = false;
-      }
-      for(var i in m) {
-        var removecomma = m[i].replace(/,\s*$/,'');
-        var remove1stquote = removecomma.replace(/^\s*"/,'');
-        var removelastquote = remove1stquote.replace(/"\s*$/,'');
-        var removeescapedquotes = removelastquote.replace(/\\"/,'"');
-        result.push(removeescapedquotes);
-      }
-      var tr = error? '<tr class="error">' : '<tr>';
-      r.push(tr+_.template(row, {items : result})+"</tr>");
-    }
-    else {
-      error = true;
-    }
-  }
-  r.unshift('<p>\n<table class="center" id="result">');
-  r.push('</table>');
-  //alert(r.join('\n')); // debug
-  finaltable.innerHTML = r.join('\n');
-}
 
 window.onload = function() {
   // If the browser supports localStorage and we have some stored data
