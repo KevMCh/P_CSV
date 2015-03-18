399fd0e9e13aa9854986447a417a12a585cc3228
Adrian <acglez10@gmail.com>
2015-03-18 18:52:53 +0000

    Arreglo servidor.

--- app.js
+++ app.js
@@ -20,55 +20,6 @@ app.get('/', function (req, res) {
   res.render('index', { title: 'P_CSV' });
 })
 
-app.get('/', function (req, res) {
-  var result;
-  var original = document.getElementById("original");
-  var temp = original.value;
-  var regexp = /\s*"((?:[^"\\]|\\.)*)"\s*,?|\s*([^,]+),?|\s*,/g;
-  var lines = temp.split(/\n+\s*/);
-  var commonLength = NaN;
-  var r = [];
-  // Template using underscore
-  /*var row = "<% _.each(items, function(name) { %>"     +
-            "                    <td><%= name %></td>" +
-            "              <% }); %>";
-*/
-  var row = document.getElementById("tabla");
-  for(var t in lines) {
-    var temp = lines[t];
-    var m = temp.match(regexp);
-    var result = [];
-    var error = false;
-
-    if (m) {
-      if (commonLength && (commonLength != m.length)) {
-        alert('ERROR! row <'+temp+'> has '+m.length+' items!');
-        error = true;
-      }
-      else {
-        commonLength = m.length;
-        error = false;
-      }
-      for(var i in m) {
-        var removecomma = m[i].replace(/,\s*$/,'');
-        var remove1stquote = removecomma.replace(/^\s*"/,'');
-        var removelastquote = remove1stquote.replace(/"\s*$/,'');
-        var removeescapedquotes = removelastquote.replace(/\\"/,'"');
-        result.push(removeescapedquotes);
-      }
-      var tr = error? '<tr class="error">' : '<tr>';
-      r.push(tr+_.template(row, {items : result})+"</tr>");
-    }
-    else {
-      error = true;
-    }
-  }
-  r.unshift('<p>\n<table class="center" id="result">');
-  r.push('</table>');
-  //alert(r.join('\n')); // debug
-  finaltable.innerHTML = r.join('\n');
-});
-
 var server = app.listen(app.get('port'), function () {
 
   var host = server.address().address
